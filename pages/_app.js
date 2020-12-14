bb11b3979e243b0fd5f8c85969de46875fcde404
mikeyrayvon <mikey@interglobal.vision>
2020-12-14 16:06:38 +0100

    add discipline calling card

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import 'styles/index.css'
 
 function MyApp({ Component, pageProps }) {
+
+  console.log('%cDiscipline', 'background: #FFF; color: #000; font-family: sans-serif; padding: 1em; font-size: 2em')
+
   return (
     <Component {...pageProps} />
   )
