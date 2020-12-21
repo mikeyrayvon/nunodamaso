83723b4bc4c11033754f8dd740cbfde79e4928cb
mikeyrayvon <mikey@interglobal.vision>
2020-12-21 21:57:29 +0100

    add mailchimp form

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,7 @@
 import Header from './Header'
 import Footer from './Footer'
 import ExitPreviewButton from './ExitPreviewButton'
+import MailchimpForm from './MailchimpForm'
 
 const Layout = ({ settings, isPreview, children, altLangs, lang }) => {
   return (
@@ -13,6 +14,7 @@ const Layout = ({ settings, isPreview, children, altLangs, lang }) => {
       />
       <main className='relative flex-grow'>{children}</main>
       {isPreview ? <ExitPreviewButton /> : null}
+      <MailchimpForm settings={settings} />
       <Footer settings={settings} />
     </div>
   )
--- components/MailchimpForm.js
+++ components/MailchimpForm.js
@@ -1,48 +1,61 @@
 import React from 'react'
 import MailchimpSubscribe from 'react-mailchimp-subscribe'
+import { useTranslation } from 'react-i18next'
 
-const MailchimpForm = ({ mailchimpAction }) => {
+import Container from './Container'
 
-  let email
+const MailchimpForm = ({ settings }) => {
+  const { t } = useTranslation()
 
-  const submit = () => {
-    return email &&
-    email.value.indexOf("@") > -1 &&
-    onValidated({
-      EMAIL: email.value
-    })
+  if (settings && settings.data) {
+    if (settings.data.mailchimp_action) {
+      let email
+      const submit = (subscribe) => {
+        return email &&
+        email.value.indexOf("@") > -1 &&
+        subscribe({
+          EMAIL: email.value
+        })
+      }
+
+      return (
+        <section className='bg-blue-light py-40'>
+          <Container>
+            <MailchimpSubscribe
+              url={settings.data.mailchimp_action}
+              render={({ subscribe, status, message }) => (
+                <div className='flex -mx-6 justify-center'>
+                  <div className='w-full sm:w-2/3 md:w-7/12 lg:w-1/2 px-6 relative'>
+                    <div className='mb-14'>
+                      <p className='text-3xl text-gray'>If you would like to recieve occasional updates about upcoming classes and events, please subscribe to our newsletter.</p>
+                    </div>
+                    <div className='flex mb-4'>
+                      <input
+                        ref={node => (email = node)}
+                        type='email'
+                        className='text-xl py-2 focus:outline-none focus:border-blue font-normal border-b border-black flex-grow bg-transparent'
+                      />
+                      <button className='focus:outline-none ml-4 button py-2 hover:text-blue text-black' onClick={() => submit(subscribe)}>
+                        <span className="font-normal text-xl bg-transparent">{ t('subscribe') }</span>
+                      </button>
+                    </div>
+                    <div className='absolute w-full sm:w-2/3 rich-text'>
+                      {status === "sending" && <div>{ t('sending') }...</div>}
+                      {(status === "error" || status === "success") && (
+                        <div dangerouslySetInnerHTML={{ __html: message }} />
+                      )}
+                    </div>
+                  </div>
+                </div>
+              )}
+            />
+          </Container>
+        </section>
+      )
+    }
   }
 
-  return (
-    <div>
-      <MailchimpSubscribe
-        url={mailchimpAction}
-        render={({ subscribe, status, message }) => (
-          <div>
-            <div className='flex flex-row space-x-4 mb-4'>
-              <input
-                ref={node => (email = node)}
-                type='email'
-                className='font-normal border-b border-black flex-grow bg-transparent'
-              />
-              <button className='button py-1' onClick={submit}>
-                <span className="font-normal text-xl bg-transparent">Subscribe</span>
-              </button>
-            </div>
-            <div>
-              {status === null && <div>&nbsp;</div>}
-              {status === "sending" && <div>Sending...</div>}
-              {(status === "error" || status === "success") && (
-                <div
-                  dangerouslySetInnerHTML={{ __html: message }}
-                />
-              )}
-            </div>
-          </div>
-        )}
-      />
-    </div>
-  )
+  return null
 }
 
 export default MailchimpForm
