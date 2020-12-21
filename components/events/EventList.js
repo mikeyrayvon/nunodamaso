f7e76a3fc30e700fafeb02ac239984abb086e790
mikeyrayvon <mikey@interglobal.vision>
2020-12-22 00:18:27 +0100

    fix typo

--- components/events/EventList.js
+++ components/events/EventList.js
@@ -7,7 +7,7 @@ const EventList = ({ docs }) => {
   return (
     <Container>
       <div className='flex flex-wrap -mx-6'>
-        {docs.map((doc) => (
+        {docs.map(doc => (
           <PostListItem doc={doc} key={doc.id} />
         ))}
       </div>
