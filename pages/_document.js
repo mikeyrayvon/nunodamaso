b627856f403fb2f80e3215776d2519246db1f9b8
mikeyrayvon <mikey@interglobal.vision>
2021-04-01 15:13:13 +0200

    remove viewport meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,6 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <link rel="icon" href="/favicon.png" type="image/png" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body>
           <Main />
